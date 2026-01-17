1409b008ff23955a5af87a22f2920b4be4a9b59c
sumit <sumitjaiwal7870@gmail.com>
2026-01-17 05:36:48 +0530

    a

--- initialize.php
+++ initialize.php
@@ -1,19 +1,21 @@
 <?php
 $dev_data = array('id' => '-1', 'firstname' => 'Developer', 'lastname' => '', 'username' => 'dev_oretnom', 'password' => '5da283a2d990e8d8512cf967df5bc0d0', 'last_login' => '', 'date_updated' => '', 'date_added' => '');
 if (!defined('base_url'))
-    define('base_url', 'http://localhost:8001/');
+    define('base_url', 'https://lead1.aurifie.com/');
 if (!defined('base_app'))
     define('base_app', str_replace('\\', '/', __DIR__) . '/');
 if (!defined('dev_data'))
     define('dev_data', $dev_data);
 if (!defined('DB_SERVER'))
-    define('DB_SERVER', "localhost");
+    define('DB_SERVER', "127.0.0.1");
 if (!defined('DB_USERNAME'))
-    define('DB_USERNAME', "u385867362_lead");
+    define('DB_USERNAME', "u828453283_lead1");
 if (!defined('DB_PASSWORD'))
-    define('DB_PASSWORD', "Sumit@787870");
+    define('DB_PASSWORD', "Sumit@78787");
 if (!defined('DB_NAME'))
-    define('DB_NAME', "u385867362_lead");
+    define('DB_NAME', "u828453283_lead1");
+
+
 if (!defined('CALL_UPLOAD_API_KEY'))
     define('CALL_UPLOAD_API_KEY', "Sup3rS3cur3K3y!2024");
 if (!defined('SHEET_ACCESS_PASSWORD'))
